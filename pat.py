53c026d2fdce1b787b3d589afa2efab5dfa4a291
Chetan Jha <chetanjha888@gmail.com>
2020-12-08 14:35:54 +0530

    intial commit

---
+++ pat.py
@@ -0,0 +1,13 @@
+def pattern(n):
+    for i in range(1,n):
+       for j in range(1,i+1):
+           print("* ",end=' ')
+       print("\r")
+    for i in range(n,0,-1):
+        for j in range(0,i):
+            print("* ",end=' ')
+        print("\r")
+      
+n=5
+pattern(n)
+            
